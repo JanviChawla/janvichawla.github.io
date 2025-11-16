fad9d76f745241e22ee763058bc0cff7a16e3599
Janvi <janvi.chawla@gmail.com>
2025-11-16 17:22:15 -0500

    Quartz sync: Nov 16, 2025, 5:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Cormorant Garamond", // Cormorant Garamond
-        body: "Crimson Pro", // Crimson Pro, Ibarra Real Nova
+        body: "EB Garamond", // Crimson Pro, Ibarra Real Nova
         code: "Red Hat Mono",
       },
       colors: {
